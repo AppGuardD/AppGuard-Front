faab5f9fb80050cfb640280228d04250ea5dc9c4
JuanBaut <deserd@protonmail.com>
2024-01-26 11:44:01 -0500

    inicio de cards

--- front-end/src/views/Mangrullos/Mangrullos.tsx
+++ front-end/src/views/Mangrullos/Mangrullos.tsx
@@ -1,4 +1,5 @@
 import type React from "react"
+import Cards from "@/components/Cards/Cards"
 import { Link } from "react-router-dom"
 
 interface MangrullosComponents {
@@ -8,28 +9,13 @@ interface MangrullosComponents {
   image: string
 }
 
-const Mangrullos: React.FC<MangrullosComponents> = ({
-  name,
-  zone,
-  description,
-  // image,
-}) => {
+const Mangrullos: React.FC<MangrullosComponents> = () => {
   return (
     <div>
       <Link to={`/home`}>
         <button> Ir atras </button>
       </Link>
-      <Card>
-        <p>Name: {name}</p>
-        <p>Zone: {zone}</p>
-        <p>Descriptión: {description}</p>
-        {/* <img src={image} alt={name} /> */}
-        <Link to={`/mangrullos/detail`}>
-          <button>
-            <p>Conocer más...</p>
-          </button>
-        </Link>
-      </Card>
+      <Cards />
     </div>
   )
 }
